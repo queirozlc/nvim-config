6b5799ff57df3840895e20be09de0d615d790c47
lucasqueiroz <sepulchrolucas@gmail.com>
2024-03-07 23:34:29 -0300

    Add pre-save cleanup by removing unused imports to optimize codebase.
    Signed-off-by: lucasqueiroz <sepulchrolucas@gmail.com>

--- lua/user/autocmds.lua
+++ lua/user/autocmds.lua
@@ -86,9 +86,6 @@ vim.api.nvim_create_autocmd({ "CursorHold" }, {
 vim.api.nvim_create_autocmd({"BufWritePre"}, {
   pattern = {"*.ts", "*.tsx", "*.js", "*.jsx"},
   callback = function()
-    vim.cmd [[
-      TSToolsOrganizeImports
-      TSToolsAddMissingImports
-    ]]
+    vim.cmd [[ TSToolsRemoveUnusedImports ]]
   end
 })
